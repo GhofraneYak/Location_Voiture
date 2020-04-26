d6ac7d29ef799905c38c200152dce0bbab3ea777
OmaymaMahjoub <59088638+OmaymaMahjoub@users.noreply.github.com>
2020-04-26 23:30:46 +0100

    Update agence.h

--- agence.h
+++ agence.h
@@ -9,7 +9,7 @@ using namespace std;
 class agence
 {
     private:
-
+        list<voiture> liste_voitures;
         list<parking> liste_parking;
         list<client> liste_client;
 
@@ -64,8 +64,16 @@ class agence
     voiture wanted_car();    
     void creer_contrat();
         
-
-
-
+/*************************Methodes liste de voitures *****************/
+    void lire_fichier_voiture(string fichier_voitures);//appeler cette methode dans le constructeur d'agence
+    voiture nouvelle_voiture();
+    voiture la_voiture_plus_ancienne();
+    voiture la_voiture_plus_loue();
+    list<voiture> voitures_2_3();//dont l'age entre 2 et 3ans
+    list<voiture> voiture_actuellement_loue();//get liste voiture loue
+    list<voiture> tri_voitures_age();
+    list<voiture> tri_voiture_categorie();
+    list<voiture> liste_voiture_available(date d1,date d2);// pour avoir les voitures disponible dans une periode precise
+    void sauvegarder_liste_voiture(string fichier_voiture);
 };
 

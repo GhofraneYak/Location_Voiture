1ae98e7d74e7c598ec056eb74d6b45457d2a26b9
moenesbs <63344830+moenesbs@users.noreply.github.com>
2020-04-27 07:42:10 +0200

    Update parking.cpp

--- parking.cpp
+++ parking.cpp
@@ -1,4 +1,3 @@
-
 #include "parking.h";
 using namespace std;
 
@@ -7,12 +6,14 @@ using namespace std;
         id++;
         nbP++;
         capacite=c;
+        nbV=0;
     }
 
-    parking::parking(int id,int c, list<Voiture> liste)
+    parking::parking(int id,int c,int nb, list<Voiture> liste)
     {
         capacite=c;
         this.id=id;
+        nbV=nb;
         list<Voiture>::iterator it;
         for(it=liste.begin();it!=liste.end();it++)
         {
@@ -23,11 +24,7 @@ using namespace std;
 
     list<Voiture> parking::get_voitures_de_parking()
     {
-        list<Voiture>::iterator it;
-        for(it=liste_voiture.begin();it!=liste_voiture.end();it++)
-        {
-            return((*it).get_matricule());
-        }
+            return(liste_voiture);
     }
 
 
@@ -36,22 +33,26 @@ using namespace std;
 
     {
         cout<<"Le parking numéro"<<id<<"a comme capacité:"<<capacite<<" et contient les immatricules des voitures suivantes :"<<endl;
-        iterator it;
-        int S=0;
+
         list<Voiture>::iterator it;
-        for(it=liste_voiture.begin();it!=liste_voiture.end();it++)
+        for(it=parking::liste_voiture.begin();it!=parking::liste_voiture.end();it++)
         {
-            cout<<it.get_matricule()<<endl;
-            S++;
+            cout<<it.get_immatricule()<<endl;
+
         }
-        if (S==capacite)
+        if (this.nbV==capacite)
         {
             cout<<"Ce parking est plein"<<endl;
         }
         else
         {
-            cout<<"Vous pouvez toujours ajouter "<<(capacite-S)<<"voitures"<<endl;
+            cout<<"Vous pouvez toujours ajouter "<<(capacite-nbV)<<"voitures"<<endl;
         }
+    }
+
+    int parking::get_nbV()
+    {
+        return(nbV);
     }
 
     int parking::get_capacite()
@@ -59,7 +60,7 @@ using namespace std;
         return(capacite);
     }
 
-    void parking::set_capacite(int c )
+    void parking::set_capacite(int c)
     {
         capacite=c;
     }
@@ -68,3 +69,78 @@ using namespace std;
     {
         return(id);
     }
+
+      void parking::ajout_voiture(voiture V)
+    {
+        bool test;
+
+            if  (parking_disponible()==false)
+            {
+                cout<<"Le parking est plein"<<endl;
+            }
+            else
+            {
+                liste_voiture.push_back(V);
+            }
+
+
+    }
+
+    bool parking::parking_disponible()
+    {
+
+        return((capacite>nbV);
+    }
+
+    bool parking::voiture_est_dans_ce_parking(voiture V)
+    {
+
+            list<voiture>::iterator l;
+            for(l=parking::liste_voiture.begin();l!=parking::liste_voiture.end();l++)
+            {
+                if((*l)==V)
+                    return(true);
+            }
+            return(false);
+    }
+
+   float parking::moy_prix()
+   {
+       int S=0;
+       list<Voiture>::iterator it;
+       for(it=liste_voiture.begin();it!=liste_voiture.end();it++)
+       {
+           S+=((*it).get_prix());
+       }
+       return(S/nbv);
+   }
+
+   void parking::delete_voiture(Voiture V)
+   {
+       list<Voiture>::iterator it;
+       for(it=liste_voiture.begin();it!=liste_voiture.end();it++)
+       {
+            if ((*it)==V)// lezm surchage ?
+            {
+                liste_voiture.erase((*it));
+            }
+
+       }
+   }
+
+   void parking::vider_le_parking()
+   {    string test;
+
+       cout<<"Merci de confirmer le vidage du parking"<<endl;
+       cin>>test;
+       if (test="Oui")
+       {
+            list<Voiture>::iterator it;
+            for(it=liste_voiture.begin();it!=liste_voiture.end();it++)
+            {
+                liste_voiture.erase((*it));
+            }
+        }
+
+   }
+

70a8310b881ebb177eafc1703bc6fcac3b39e29e
GhofraneYak <63368212+GhofraneYak@users.noreply.github.com>
2020-04-29 00:01:04 +0100

    Update parking.h

--- parking.h
+++ parking.h
@@ -1,4 +1,5 @@
 #include "voiture.h"
+#include <iostream>
 
 using namespace std;
 
@@ -8,22 +9,22 @@ using namespace std;
         int id;
         int capacite;
         int nbV;
-        list<Voiture> liste_voiture;
+        list<voiture> liste_voiture;
 
     public:
 
-        parking (int id, int c, int nb,  list<Voiture> liste) ;// re-creation des parking à partir du
-        list<Voiture> get_voitures_de_parking();
+        parking (int id, int c, int nb,  list<voiture> liste) ;// re-creation des parking à partir du
+        list<voiture> get_voitures_de_parking();
         int get_capacite();
         void set_capacite(int c);
         int get_nbV();
         int get_id();
         void affichage_info();
         bool parking_disponible();
-        bool voiture_est_dans_ce_parking(Voiture V);
-        void ajout_voiture(Voiture v);
+        bool voiture_est_dans_ce_parking(voiture V);
+        void ajout_voiture(voiture v);
         float moy_prix();
-        void delete_voiture(Voiture V)/
+        void delete_voiture(voiture V)/
         void vider_le_parking();
 
     };

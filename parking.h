4c0fe8ac9b787bf024a1b85783b2bb5819213b99
moenesbs <63344830+moenesbs@users.noreply.github.com>
2020-04-30 00:44:16 +0200

    Update parking.h

--- parking.h
+++ parking.h
@@ -7,21 +7,21 @@ using namespace std;
     {
         int id;
         int capacite;
-        int nbV;
+        int nbV;   // Nombre des voitures déja présente dans le parking
         list<voiture> liste_voiture;
 
     public:
         static int nbP;
         parking (int id, int c, int nb,  list<voiture> liste) ;// re-creation des parking à partir du
-        parking (int c);
+        parking (int c);  // création d'un nouveau parking
         list<voiture> get_voitures_de_parking();
         int get_capacite();
         void set_capacite(int c);
         int get_nbV();
         int get_id();
         void affichage_info();
-        bool parking_disponible();
-        bool voiture_est_dans_ce_parking(voiture);
+        bool parking_disponible();  
+        bool voiture_est_dans_ce_parking(voiture); // Recherche d'une voiture dans ce parking
         void ajout_voiture(voiture);
         float moy_prix();
         void delete_voiture(voiture);

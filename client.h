e51ff6f3ef3ffbb3cf22c6e2ac0702cb3321da1b
GhofraneYak <63368212+GhofraneYak@users.noreply.github.com>
2020-05-04 21:59:21 +0100

    Update client.h

--- client.h
+++ client.h
@@ -10,13 +10,16 @@ class client
         string nom;
         long id;
         date dernier_date_loc;
+        int nbr_loc;
 
     public:
-        client(string n, long idd, date d);
+        client(string n, long idd, date d, int nbr);
         string getnom();
         long getid();
         date getdate();
         void setdate(date d);
+        int get_nbr_loc();
+        void set_nbr_loc();
 
         friend ostream& operator<<(ostream& sortie, client& cl);
 };
